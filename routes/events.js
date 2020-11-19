65b0d0e500846e4c0ac170f8b6c21928a17fb38e
Kevin Lam <kevin.lam13@hotmail.com>
2020-11-19 17:22:09 -0600

    Primer commit

---
+++ routes/events.js
@@ -0,0 +1,36 @@
+const { Router } = require("express");
+const {check} = require("express-validator");
+const router = Router();
+
+
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require("../controllers/events");
+const {isDate} = require("../helpers/isDate");
+const {validarCampos} = require("../middlewares/validar-campos");
+const {validarJWT} = require("../middlewares/validar-jwt");
+
+
+//Aplicar midleware a varias rutas
+router.use(validarJWT);
+
+// Obtener eventos
+router.get('/', getEventos);
+
+//Crear un nuevo evento
+router.post(
+    '/',  
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validarCampos
+    ],
+    crearEvento,
+);
+
+//Actualizar evento
+router.put('/:id', actualizarEvento);
+
+//Borrar evento
+router.delete('/:id', eliminarEvento);
+
+module.exports = router;
